c988345628c1aad88e9e72d4532632f8302c8f69
Francisco Dala <killerbless12@gmail.com>
2026-09-12 22:57:38 +0100

    feat: add page login

--- apps/web/src/app/login/Register.tsx
+++ apps/web/src/app/login/Register.tsx
@@ -91,7 +91,7 @@ export default function Register() {
                         </div>
                     </div>
 
-                    <div className="border-t pt-4 space-y-4">
+                    <div className="border-t pt-4 space-y-0">
                         <h2 className="text-xs font-semibold text-gray-900 uppercase tracking-wider">Administrador</h2>
 
                         <div>

58f735116491f85477dc486953d0559a1efbec74
Francisco Dala <killerbless12@gmail.com>
2026-09-13 23:42:19 +0100

    update: salvar cliente 13/09/26 as 23:42m

--- apps/api/app/db/session.py
+++ apps/api/app/db/session.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.db.base import Base # <- usa o teu base.py
 from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL.replace("+asyncpg", "") # tira asyncpg da url
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL, # <- Agora já vem certa do .env
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()

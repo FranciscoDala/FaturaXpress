5cf0ee6abb3e37386c11147881e24f27a72f0a6d
Francisco Dala <killerbless12@gmail.com>
2026-09-13 23:13:34 +0100

    update: salvar cliente 13/09/26 as 23:13m

--- apps/api/app/db/session.py
+++ apps/api/app/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from apps.api.app.core.config import settings # <-- pega do teu config
+from app.core.config import settings # <-- pega do teu config
 
 engine = create_engine(settings.DATABASE_URL) # <-- usa o settings
 

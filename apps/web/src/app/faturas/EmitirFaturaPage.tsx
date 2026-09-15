70ce3cd6eb37ecaa3f6d1f486c5426b7729da7b7
Francisco Dala <killerbless12@gmail.com>
2026-09-15 22:22:59 +0100

    update: ajuste de fatura 15/09/26 as 22:22m

--- apps/web/src/app/faturas/EmitirFaturaPage.tsx
+++ apps/web/src/app/faturas/EmitirFaturaPage.tsx
@@ -85,8 +85,8 @@ export default function EmitirFaturaPage() {
                     </div>
                 </div>
 
-                {/* CONTEÚDO DE BAIXO - MESMO LIMITE DO DE CIMA */}
-                <div className="px-4 sm:px-8 lg:px-12 py-6 bg-white">
+                {/* CONTEÚDO DAS ABAS - MESMO WIDTH DO HEADER */}
+                <div className="w-full px-4 sm:px-8 lg:px-12 py-6">
                     {activeTab === 'emitir' && <TabEmitir clienteId={clienteId!} onEmitida={() => { setActiveTab('curso'); fetchFaturas() }} />}
                     {activeTab === 'curso' && <TabCurso faturas={faturasCurso} cliente={cliente} empresa={empresa} onRefresh={fetchFaturas} />}
                     {activeTab === 'emitidas' && <TabEmitidas faturas={faturasEmitidas} cliente={cliente} empresa={empresa} />}

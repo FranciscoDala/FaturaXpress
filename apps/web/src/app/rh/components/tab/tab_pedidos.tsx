63596e354decaa8e04c515f70606419189066d62
Francisco Dala <killerbless12@gmail.com>
2026-09-20 10:59:57 +0100

    update: ajuste da page RH, modal_func  20/09/26 as 10:59m

--- apps/web/src/app/rh/components/tab/tab_pedidos.tsx
+++ apps/web/src/app/rh/components/tab/tab_pedidos.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { api } from '../../../lib/api'
+import { api } from '../../../../lib/api'
 import { toast } from 'sonner'
 
 export default function TabPedidos(){
